001f56516ffe5af463587fdf48ee3438eda8a8db
Esoteric925 <haval925@gmail.com>
2017-02-22 16:54:07 +0100

    added distributor component

---
+++ server/src/main/java/se/kth/id2203/DistributorComponent.java
@@ -0,0 +1,10 @@
+package se.kth.id2203;
+
+import se.sics.kompics.ComponentDefinition;
+
+/**
+ * Created by Amir on 2017-02-22.
+ */
+public class DistributorComponent extends ComponentDefinition {
+
+}
